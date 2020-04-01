b8568d324d9089336471f5030fb064db3b151c49
Plabrum <plabrum@college.harvard.edu>
2020-04-01 19:37:48 -0400

    not compiling - matrix object incomplete

--- strassen.cpp
+++ strassen.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <time.h> 
 using namespace std;
 
 // after this point normal matrix operation will take over
@@ -6,16 +7,53 @@ int crossover = 128;
 int dimension = 2048;
 int current_dim = dimension;
 
+// NOTES
+// Change to be able to handle 32 bit numbers - perhaps use long int
+
+class Matrix
+{
+	public:
+		int dims = 0;
+		// int output_matrix;
+
+		Matrix(int dimension){
+			int dims = dimension;
+			int output_matrix[dims][dims];
+		}
+		
+
+		void initialise(){
+			for (int i=0; i < dims; i++){
+				for (int j =0; j < dims; j++){
+					output_matrix[i][j] = rand() %1;
+				}
+			}
+		}
+
+		void print_matrix(){
+			for (int i = 0; i < dims; i++){
+				for (int j = 0; j < dims; j++){
+					cout << output_matrix[i][j] << " ";
+				}
+				cout << "\n";
+			}
+		}
+
+};
+
 int create_matrix (){
+	int output_matrix[dimension][dimension];
+	for (int i=0; i < dimension; i++){
+		for (int j =0; j < dimension; j++){
+			output_matrix[i][j] = rand() %1;
+		}
+	}
 	// create matrix
 	return 0;
 }
 
-int conventional (int val){
+int conventional (){
 	// perform conventional matrix multi
-	if (val > 1){
-		return 1;
-	}
 	return 0;
 }
 
@@ -24,14 +62,13 @@ int strass(int val){
 		//perform strassen
 		strass(val);
 	}
-	conventional(1);
+	conventional();
 	return 0;
 }
 
 
-
 int main(){
-	cout << "hello, world!";
+	cout << "hello, world! \n";
 	return 0;
 }
 // generates the matrices based on a given size, 
