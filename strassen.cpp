f3f09c4e877f3d96610712d08d59541586079021
Plabrum <plabrum@college.harvard.edu>
2020-03-31 22:18:52 -0400

    skeleton code

--- strassen.cpp
+++ strassen.cpp
@@ -1,6 +1,39 @@
 #include <iostream>
 using namespace std;
 
+// after this point normal matrix operation will take over
+int crossover = 128;
+int dimension = 2048;
+int current_dim = dimension;
+
+int create_matrix (){
+	// create matrix
+	return 0;
+}
+
+int conventional (int val){
+	// perform conventional matrix multi
+	if (val > 1){
+		return 1;
+	}
+	return 0;
+}
+
+int strass(int val){
+	while (current_dim > crossover){
+		//perform strassen
+		strass(val);
+	}
+	conventional(1);
+	return 0;
+}
+
+
+
+int main(){
+	cout << "hello, world!";
+	return 0;
+}
 // generates the matrices based on a given size, 
 
 
@@ -8,8 +41,3 @@ using namespace std;
 
 
 // write a diagonaliser output
-
-int main(){
-	cout << "hello, world!";
-	return 0;
-}
