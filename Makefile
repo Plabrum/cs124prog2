0f2b50b5b4c0847fbebcfa580b2c64e922cddeaf
Plabrum <plabrum@college.harvard.edu>
2020-03-31 22:02:22 -0400

    added makefile, hello world

---
+++ Makefile
@@ -0,0 +1,2 @@
+all: strassen.cpp
+	g++ -std=c++17 -Wall -Wextra strassen.cpp -o strassen
---
+++ strassen.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+// generates the matrices based on a given size, 
+
+
+// run strassen until a cutoff point n after that normal matrix
+
+
+// write a diagonaliser output
+
+int main(){
+	cout << "hello, world!";
+	return 0;
+}
