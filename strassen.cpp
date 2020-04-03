5e0fa3df9351dbe495482e6267e5b7f36805b89b
Plabrum <plabrum@college.harvard.edu>
2020-04-02 22:19:28 -0400

    working read in for all

--- strassen.cpp
+++ strassen.cpp
@@ -7,7 +7,7 @@ using namespace std;
 // after this point normal matrix operation will take over
 int crossover = 128;
 // int dimension = 2048;
-int dimension = 16;
+int dimension = 4;
 int current_dim = dimension;
 
 // NOTES
@@ -20,9 +20,9 @@ class Matrix
 		int array_len = 0;
 		int *data;
 
-		Matrix(int size){
-			dims = size;
-			array_len = pow(dims,2);
+		Matrix(int array_size){
+			array_len = array_size;
+			dims = (int) sqrt(array_size);
 			data = new int[array_len];
 		}
 		
@@ -65,8 +65,26 @@ class Matrix
 			cout << "\n";
 		}
 
-		void read(int* array_in){
-			this->data = array_in;
+		void read(int* array_in, string option){
+			if (option == "first"){
+				for (int i=0; i < array_len; i++){
+					this->data[i] = array_in[i+1];
+				}
+				
+			}
+			else if (option == "second"){
+				for (int i = 0; i < array_len; i++){
+					this->data[i] = array_in[i+array_len+1];
+				}
+			}
+			else if (option == "all"){
+				for (int i=0; i < array_len; i++){
+					this->data[i] = array_in[i+1];
+				}
+			}
+			else{
+				cout << "incorrect read parameters given \n";
+			}
 		}
 
 };
@@ -74,38 +92,35 @@ int* read_in(string filename){
 	fstream in_file(filename);
 	if (in_file.is_open())
 	{	string line;
-		int rows = 0;
+
 		//Count how many rows in input file
-		while (getline(in_file,line)){	
+		int rows = 0;
+		while (getline(in_file,line)){
 			rows++;
 		}
+		// set the first value of the array to be the array length
+		// assumes that all arrays are square
+		int *data_all = new int[rows+1];
+		data_all[0] = (int) (rows/2);
 
-		int *data_a = new int[rows/2];
-		int *data_b= new int[rows/2];
+		// move seeker to start
+		in_file.clear();
+		in_file.seekg(0);
 
-		for (int i =0; i < rows/2; i++){
+		for (int i =1; i < rows+1; i++){
 			getline (in_file, line);
-			data_a[i] = stoi(line);
+			data_all[i] = stoi(line);
 		}
-		for (int i = rows/2; i < rows; i++){
-			getline (in_file, line);
-			data_b[i] = stoi(line);
-		}
+
+
+		// Close file because we have already stored the array
 		in_file.close();
-
-		// hacky stuff here to get an array of pointers
-		int* dim_ptr = nullptr;
-		*dim_ptr = (int) sqrt(rows/2);
-		int* arr_data[3]{dim_ptr, data_a, data_b};
-		int* arr_ptr = &arr_data[0];
-		return arr_ptr;
+		return data_all;
 	}
 	else {
 		cout << "Unable to open file";
 		return NULL;
 	}
-
-	
 }
 
 Matrix conventional(Matrix a, Matrix b){
@@ -113,8 +128,19 @@ Matrix conventional(Matrix a, Matrix b){
 	assert(a.dims == b.dims);
 	Matrix output_matrix(a.dims);
 
-	for (int i = 0; i < output_matrix.array_len; i++){
-		output_matrix.data[i] = a.data[i] + b.data[i];
+	// iterate through the output matrix
+	for (int i = 0; i < output_matrix.dims; i++){
+		for (int j = 0; j < output_matrix.dims; j++){
+			int sum =0;
+			for (int k = 0; k < output_matrix.dims; k++){
+				// I val
+				int i_val = i*output_matrix.dims + k;
+				int j_val = (i+k)*output_matrix.dims + j;
+				sum += i_val*j_val;
+			}
+			cout << "for i = " << i << " for j = " << j << " Sum: " << sum << "\n";
+			output_matrix.data[(i*output_matrix.dims) +j] = sum;
+		}
 	}
 	return output_matrix;
 }
@@ -127,38 +153,65 @@ int strass(int val){
 	return 0;
 }
 
-int triangleCount(int probability){
-    //initialize a random 1024x1024 adjacency matrix
-    Matrix mat(1024); //we hardcode this 
-    mat.init_randadjacency(probability);
-    2mat = strass(mat, mat);
-    3mat = strass(2mat, mat);
-    //count number of triangles
-    int diagonaledges = 0;
-    for (int i = 0; i < array_len; i = i + array_len + 1){
-        //We only hit the diagonal entries by adding dim + 1 to i each time
-        if (data[i] == 1){
-            diagonaledges = diagonaledges + 1;
-        }
-    }
-    int triangles = diagonaledges / 6;
-    print(triangles)
-    return (triangles);
+bool matrix_equal(Matrix a, Matrix b){
+	
 }
 
+// int triangleCount(int probability){
+//     //initialize a random 1024x1024 adjacency matrix
+//     Matrix mat(1024); //we hardcode this 
+//     mat.init_randadjacency(probability);
+//     2mat = strass(mat, mat);
+//     3mat = strass(2mat, mat);
+//     //count number of triangles
+//     int diagonaledges = 0;
+//     for (int i = 0; i < array_len; i = i + array_len + 1){
+//         //We only hit the diagonal entries by adding dim + 1 to i each time
+//         if (data[i] == 1){
+//             diagonaledges = diagonaledges + 1;
+//         }
+//     }
+//     int triangles = diagonaledges / 6;
+//     cout << triangles;
+//     return (triangles);
+// }
+
 int main(){
-	Matrix mat_a(dimension);
-	Matrix mat_b(dimension);
-	mat_a.init_rand();
-	mat_b.init_rand();
-	mat_a.print_matrix();
-	mat_b.print_matrix();
-	conventional(mat_a, mat_b).print_matrix();
+	cout << "conventional test: \n\n";
+	// Matrix mat_a(dimension);
+	// Matrix mat_b(dimension);
+	// mat_a.init_rand();
+	// mat_b.init_rand();
+	// mat_a.print_matrix();
+	// mat_b.print_matrix();
+	
 
 	int* data_ptr = read_in("ascii_file.txt");
-	Matrix mat_c(*data_ptr[0]);
-	mat_c.read(*data_ptr[1])
-	Matrix mat_d(dimension);
+
+	Matrix mat_c(data_ptr[0]);
+	mat_c.read(data_ptr, "first");
+	cout << "Matrix C: ";
+	mat_c.print_matrix();
+
+	Matrix mat_d(data_ptr[0]);
+	mat_d.read(data_ptr, "second");
+	cout << "Matrix D: ";
+	mat_d.print_matrix();
+
+	delete data_ptr;
+
+	cout << "\nconventional dot product: \n";
+	conventional(mat_c, mat_d).print_matrix();
+
+	int* sol_ptr = read_in("solution.txt");
+	Matrix mat_sol(sol_ptr[0]*2);
+	mat_sol.read(sol_ptr, "all");
+	cout << "Matrix Solution: ";
+	mat_sol.print_matrix();
+	delete sol_ptr;
+
+	// check correctnes against python version
+
 	return 0;
 }
 // generates the matrices based on a given size, 
